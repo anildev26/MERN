cccf9f761e4775421bf6ec43aec44a956f4ca176
anilsahudev26 <anilsahudev26@gmail.com>
2023-11-12 09:33:23 +0530

    registeration added

--- Mern Learning/server/routes/auth.js
+++ Mern Learning/server/routes/auth.js
@@ -8,32 +8,52 @@ router.get("/", (req, res) => {
     res.send("Home page from Express Router");
 })
 
-router.post("/register", (req, res) => {
-    // Destruction user data json to access it more easily
+router.post("/register", async(req, res) => {
+    // Destructuring user data json to access it more easily
     const {name, email, phone, work, password, cpassword} = req.body;
+
         // console.log(name);
         // console.log(password);
         // res.send("You have successfully registered");
         // res.json({ User1 : req.body });
+
         if(!name || !email || !phone || !work || !password || !cpassword){
             return res.status(422).json({error: "Please fill all the information"})
         }
 
-        User.findOne({email:email}) // returns a promise
-        .then((userExist) => {
-            if(userExist) {
+        // Putting all logical code in [try/catch block] any error in any phase occurs will be handled by catch here:
+        try {
+
+            const alreadyRegister = await User.findOne({email:email});  // Handle a promise
+            
+            if(alreadyRegister) {
                 return res.status(422).json({error: "Email already exist"})
-            }
-            //User : which is db model user uska instance create krke use add krna hoga
+            }  
+
             const user = new User({name, email, phone, work, password, cpassword})
-            user.save() // returns a promise
-            .then(()=>{
-                res.status(201).json({message: "User registered successfully, go to login"})
-            }).catch((err)=>{
-                res.status(500).json({err: "Failed to register user"})
-            })
-        }).catch((err)=>{ console.log(err); })
+            const registerUser = await user.save() // Handle a promise
+
+            res.status(201).json({message: "User registered successfully, go to login"})
+
+            
+        } catch(err){
+            console.log(err);
+        }
 })
 
+router.post("/signin", async (req, res) => {
+    const {email, password} = req.body;
 
+    // res.json({ "message": "Success" })
+
+    if (!email || !password){
+        return res.status(422).json({error: "Invalid Credentials"})
+    }
+
+    try {
+        
+    } catch (err) {
+        console.log(err);
+    }
+})
 module.exports = router
