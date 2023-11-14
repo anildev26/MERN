463ef369025421cbef8a9e6b933b84969dc4c1ab
anilsahudev26 <anilsahudev26@gmail.com>
2023-11-14 23:01:21 +0530

    updated

--- Mern Learning/server/routes/auth.js
+++ Mern Learning/server/routes/auth.js
@@ -1,66 +1,72 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-
-require("../db/conn")
-const User = require("../models/userSchema")
+const bcrypt = require("bcryptjs");
+require("../db/conn");
+const User = require("../models/userSchema");
 
 router.get("/", (req, res) => {
-    res.send("Home page from Express Router");
-})
+  res.send("Home page from Express Router");
+});
 
-router.post("/register", async(req, res) => {
-    // Destructuring user data json to access it more easily
-    const {name, email, phone, work, password, cpassword} = req.body;
+router.post("/register", async (req, res) => {
+  // Object Destructuring and getting user data to access it more easily
+  const { name, email, phone, work, password, cpassword } = req.body;
 
-        // console.log(name);
-        // console.log(password);
-        // res.send("You have successfully registered");
-        // res.json({ User1 : req.body });
+  // console.log(name);
+  // console.log(password);
+  // res.send("You have successfully registered");
+  // res.json({ User1 : req.body });
 
-        if(!name || !email || !phone || !work || !password || !cpassword){
-            return res.status(422).json({error: "Please fill all the information"})
-        }
+  if (!name || !email || !phone || !work || !password || !cpassword) {
+    return res.status(422).json({ error: "Please fill all the information" });
+  }
 
-        // Putting all logical code in [try/catch block] any error in any phase occurs will be handled by catch here:
-        try {
+  // Putting all logical code in [try/catch block] any error in any phase occurs will be handled by catch here:
+  try {
+    const alreadyRegister = await User.findOne({ email: email }); // Handle a promise
 
-            const alreadyRegister = await User.findOne({email:email});  // Handle a promise
-            
-            if(alreadyRegister) {
-                return res.status(422).json({error: "Email already exist"})
-            }else if(password != cpassword){
-                return res.status(422).json({error: "Password does not match"})
-            }else {
-                const user = new User({name, email, phone, work, password, cpassword})
-                //yaha pe horahi hai hashing middleware from models/userSchema
-                const registerUser = await user.save() // Handle a promise
-                res.status(201).json({message: "User registered successfully, go to login"})
-            }
-        } catch(err){
-            console.log(err);
-        }
-})
+    if (alreadyRegister) {
+      return res.status(422).json({ error: "Email already exist" });
+    } else if (password != cpassword) {
+      return res.status(422).json({ error: "Password does not match" });
+    } else {
+      const user = new User({ name, email, phone, work, password, cpassword });
+      //yaha pe horahi hai hashing middleware from models/userSchema
+      const registerUser = await user.save(); // Handle a promise
+      res.status(201).json({ message: "User registered successfully, go to login" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 router.post("/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body; // object destructuring
 
-    try {
-        const {email, password} = req.body; // object destructuring
-
-        if (!email || !password){
-            return res.status(400).json({error: "Please fill the data"})
-        }
-        
-        const userLogin = await User.findOne({ email:email });
-        console.log(userLogin); // displays the whole document of matched email
-        
-        if(!userLogin){
-            res.status(400).json({ "error":"Invalid Credentials" })
-        }else{
-            res.json({"message": "Login Successfull"})
-        }
-        
-    } catch (err) {
-        console.log(err);
+    // Case 1: If both sign-in fields are empty and user clicks on submit
+    if (!email || !password) {
+      return res.status(400).json({ error: "Please fill the data" });
     }
-})
-module.exports = router
+
+    // Case 2: It should be already registered Email and only if email entered and blank password and user clicks on submit then return error
+    const userLogin = await User.findOne({ email: email });
+
+    console.log(userLogin); // displays the whole document of matched email, if email not matched then return "NULL"
+
+    if (userLogin) {
+      // Case 3: Email found in db and user also entered the password then check for credentials if passwords matches then "Login Successfull" or else if password incorrect then "Invalid Credentials"
+      const isMatch = await bcrypt.compare(password, userLogin.password);
+      if (isMatch) {
+        res.status(200).json({ message: "Login Successfull" });
+      } else {
+        res.status(400).json({ error: "Invalid Credentials" });
+      }
+    } else {
+      res.status(400).json({ error: "Invalid Credentials" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+module.exports = router;
